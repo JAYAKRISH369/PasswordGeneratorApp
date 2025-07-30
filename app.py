1445fb6d9ff1ca36ae4a5aef4556c7d4fa2b2744
Jayakrishna Yellakara <yjkrishna@ibasis.net>
2025-07-30 18:31:29 +0530

    fix 2 on Render

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def error_page():
     return render_template("error.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
